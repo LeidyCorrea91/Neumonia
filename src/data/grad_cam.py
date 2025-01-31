61e793b9d37bf0aa5e471ec1389237a65ae878a6
LeidyCorrea91 <124936088+LeidyCorrea91@users.noreply.github.com>
2025-01-31 13:10:02 -0500

    Update grad_cam.py

--- src/data/grad_cam.py
+++ src/data/grad_cam.py
@@ -1,1 +1,29 @@
+import numpy as np
+import cv2
+import tensorflow.keras.backend as K
+
+def generate_grad_cam(image, model):
+    """Genera un mapa de calor Grad-CAM a partir de la imagen y el modelo."""
+    preds = model.predict(image)
+    argmax = np.argmax(preds[0])
+    output = model.output[:, argmax]
+    last_conv_layer = model.get_layer("conv10_thisone")
+    
+    grads = K.gradients(output, last_conv_layer.output)[0]
+    pooled_grads = K.mean(grads, axis=(0, 1, 2))
+    
+    iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
+    pooled_grads_value, conv_layer_output_value = iterate([image])
+    
+    for i in range(conv_layer_output_value.shape[-1]):
+        conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+    
+    heatmap = np.mean(conv_layer_output_value, axis=-1)
+    heatmap = np.maximum(heatmap, 0)
+    heatmap /= np.max(heatmap)
+    heatmap = cv2.resize(heatmap, (image.shape[1], image.shape[2]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    
+    return heatmap
 
